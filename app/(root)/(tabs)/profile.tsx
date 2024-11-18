502e38c8b6659ced03e2d6cc1fbdef9812acbb32
Tran Ngoc Phat <tnphat203@gmail.com>
2024-11-18 19:57:35 +0700

    fix profile ui

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Image, View, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { IInputField } from "@/types/type";
+import { InputField } from "@/components";
 const Profile = () => {
   return (
     <SafeAreaView>
-      <Text>Profile</Text>
+      <View>
+        <Text style={{
+          fontSize: 28,
+          fontWeight: 'bold',
+
+        }}>Thông tin cá nhân của bạn</Text>
+      </View>
+      <View style={{
+        width: 200,
+        height: 200,
+        backgroundColor: 'red',
+        marginLeft: 105,
+      }}>
+      </View>
+      <View style={{
+        width: '90%',
+        marginLeft: 20,
+      }}>
+        <InputField label="Họ" />
+      </View>
+
     </SafeAreaView>
   );
 };
