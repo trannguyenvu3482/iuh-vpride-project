08245ed1532541abdf46c087a3079bdb54330492
Vu Tran <21088201.vu@student.iuh.edu.vn>
2024-12-04 08:02:19 +0700

    feat: changed profile

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,32 +1,56 @@
-import React from "react";
-import { Text, Image, View, TextInput } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { IInputField } from "@/types/type";
-import { InputField } from "@/components";
+
+import InputField from "@/components/InputField";
+import { useUserStore } from "@/zustand";
+
 const Profile = () => {
+  const { user, userData } = useUserStore();
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text style={{
-          fontSize: 28,
-          fontWeight: 'bold',
+    <SafeAreaView className="flex-1 w-full">
+      <ScrollView
+        className="px-5 w-full flex-1"
+        contentContainerStyle={{ paddingBottom: 120 }}
+      >
+        <Text className="text-2xl font-JakartaBold my-5">My profile</Text>
 
-        }}>Thông tin cá nhân của bạn</Text>
-      </View>
-      <View style={{
-        width: 200,
-        height: 200,
-        backgroundColor: 'red',
-        marginLeft: 105,
-      }}>
-      </View>
-      <View style={{
-        width: '90%',
-        marginLeft: 20,
-      }}>
-        <InputField label="Họ" />
-      </View>
+        <View className="flex items-center justify-center my-5">
+          <Image
+            source={{
+              uri: "https://robohash.org/TU2.png",
+            }}
+            className=" rounded-full h-[110px] w-[110px] border-[3px] border-white shadow-sm shadow-neutral-300 bg-gray-300"
+          />
+        </View>
 
+        <View className=" bg-white rounded-lg shadow-sm shadow-neutral-300 px-5 py-3 w-full flex-1">
+          <InputField
+            className="w-full"
+            label="First name"
+            placeholder={userData?.full_name || "Không tìm thấy"}
+            containerStyle="w-full"
+            inputStyle="p-3.5"
+            editable={false}
+          />
+
+          <InputField
+            label="Email"
+            placeholder={userData?.email || "Không tìm thấy"}
+            containerStyle="w-full"
+            inputStyle="p-3.5"
+            editable={false}
+          />
+
+          <InputField
+            label="Phone"
+            placeholder={userData?.phone_number || "Không tìm thấy"}
+            containerStyle="w-full"
+            inputStyle="p-3.5"
+            editable={false}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
